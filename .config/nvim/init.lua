339a8be0d52a7dd1985920bc5fb6d052c9a2dd9f
FrostyNick <nicholasboguckimn@gmail.com>
2023-05-31 09:26:29 -0500

    autodir

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,2 @@
+vim.cmd("set autochdir") -- slightly better than default
 require("frostynick")
---print("ibxtoycat is non't")
---resume tutorial (also nvim is now sexy af) https://youtu.be/w7i4amO_zaE?t=935
