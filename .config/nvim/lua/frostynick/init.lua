9d666438e1df783d6cfe0c94170e49708d68498a
FrostyNick <57016218+FrostyNick@users.noreply.github.com>
2023-08-11 11:05:11 -0500

    Clean up lua/fro../init.lua

--- .config/nvim/lua/frostynick/init.lua
+++ .config/nvim/lua/frostynick/init.lua
@@ -1,26 +1,20 @@
+vim.opt.shada = "!,%5,'200,f1,<50,s10,h"
+require("frostynick.set")
 require("frostynick.lazy")
 require("frostynick.remap")
-require("frostynick.set")
 -- local a = math.random(6)
 local msg
 
 -- Move to new file, then .gitignore it and check in lua if it exists.
 -- if a == 1 then -- I'll add this as a joke later.. not good for a public config file by default lol
---     msg = [[Nuwuevim: sus (plugin remaps not working right now)]]
+--     msg = [[Nuwuevim: sus]]
 -- elseif a == 2 then
---     msg = [[Nuwuevim: I like your arms]]
+--     msg = [[Nuwuevim: yes]]
 -- elseif a == 3 then
---     msg = [[Nuwuevim: omg oggfgdfojgdfiogdfg HIIII =D]]
--- elseif a == 4 then
---     msg = [[Nuwuevim: Your account has been terminated for security reasons. Please enter your SSN to continue.]]
--- else
--- pcall(function()
---     print(vim.inspect(vim.version()))
---     local v = vim.version()
---     print(string.format("Neovim version: %d.%d.%d", v.major, v.minor, v.patch))
--- end)
--- check if empty buffer then vim.cmd("term")
 msg = [[Nuwuevim: ~hiii :3]]
+-- elseif a == 4 then
+--     msg = [[Nuwuevim: Your account has been terminated for security reasons. Please enter your SSN to continue exiting vim.]]
+-- else
 -- msg = [[Type <C-\><C-n> to enter insert mode.]]
 -- end
 print(msg)
