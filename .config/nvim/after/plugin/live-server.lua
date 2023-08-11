5549d4f958550299d6360c41265692d2da6eb457
FrostyNick <57016218+FrostyNick@users.noreply.github.com>
2023-08-11 11:07:18 -0500

    live-server.lua: Added debounce

--- .config/nvim/after/plugin/live-server.lua
+++ .config/nvim/after/plugin/live-server.lua
@@ -1,4 +1,13 @@
 local l = require('live-server')
 l.setup(--[[ args = {"--browser=librewolf"} ]])
+vim.g.liveservertoggle = true
 vim.keymap.set('n', '<leader>lt', l.start) -- t=true, f=false
 vim.keymap.set('n', '<leader>lf', l.stop)
+vim.keymap.set('n', '<leader>ll', function()
+    if vim.g.liveservertoggle then
+        l.start()
+    else
+        l.stop()
+    end
+    vim.g.liveservertoggle = not vim.g.liveservertoggle
+end)
