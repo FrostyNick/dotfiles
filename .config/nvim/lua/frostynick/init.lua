4f3cc82669a2dd5f0dd09da933ad04c0497e5342
FrostyNick <57016218+FrostyNick@users.noreply.github.com>
2023-08-11 14:44:22 -0500

    Cleaned nvim/frostynick/init.lua (x2)

--- .config/nvim/lua/frostynick/init.lua
+++ .config/nvim/lua/frostynick/init.lua
@@ -1,21 +1,17 @@
-vim.opt.shada = "!,%5,'200,f1,<50,s10,h"
+vim.opt.shada = "!,%5,'200,f1,<50,s10,h" -- 200 oldfiles now. :help 'shada'
+
 require("frostynick.set")
 require("frostynick.lazy")
 require("frostynick.remap")
--- local a = math.random(6)
-local msg
 
--- Move to new file, then .gitignore it and check in lua if it exists.
--- if a == 1 then -- I'll add this as a joke later.. not good for a public config file by default lol
---     msg = [[Nuwuevim: sus]]
--- elseif a == 2 then
---     msg = [[Nuwuevim: yes]]
--- elseif a == 3 then
-msg = [[Nuwuevim: ~hiii :3]]
--- elseif a == 4 then
---     msg = [[Nuwuevim: Your account has been terminated for security reasons. Please enter your SSN to continue exiting vim.]]
--- else
--- msg = [[Type <C-\><C-n> to enter insert mode.]]
--- end
-print(msg)
--- :set rnu nu
+-- Move below to frostynick.??? later 
+local msg = {"sus", "yes", "~hiii :3", "learn quickfix. nowww",
+"my purpose is to outlive you",
+"Your account has been terminated for security reasons. Please enter your SSN to continue exiting vim.",
+[[In terminal, type <C-\><C-n> to enter normal mode.]]}
+local a = math.random(#msg + 1)
+if a > #msg then
+    a = #msg
+end
+
+print("Nuwuvim: "..msg[a])
