2aca6f1e83bd719d8324fe4c3ef54fdcbd43941f
FrostyNick <57016218+FrostyNick@users.noreply.github.com>
2023-08-11 11:06:10 -0500

    Fixed LSP keybinds + many updates

--- .config/nvim/lua/frostynick/remap.lua
+++ .config/nvim/lua/frostynick/remap.lua
@@ -1,6 +1,7 @@
 -- missing: comment shortcut
 
 local k = vim.keymap
+local telescopeBi = require("telescope.builtin")
 vim.g.mapleader = ' '
 k.set("v", "J", ":m '>+1<CR>gv=gv")
 k.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -23,20 +24,34 @@ k.set("n", "Q", "<nop>")
 -- not yet. k.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 -- Diagnostic shortcuts
-local function kLsp(map, todo, desc)
+local function kLsp(map, action, desc)
     -- desc = desc or map
-    k.set("n", map, todo, {buffer=0, desc="LSP: "..desc or map})
+    k.set("n", map, action, {buffer=0, desc="LSP: "..desc or map})
 end
-kLsp("<leader>dk", vim.diagnostic.goto_prev, "prev diagnostic")
-kLsp("<leader>dj", vim.diagnostic.goto_next, "next diagnostic")
-kLsp("<leader>dl", "<cmd>Telescope disagnostics<CR>", "Telescope diagnostics")
+-- Stopped working:
+-- kLsp("<leader>dk", vim.diagnostic.goto_prev, "prev diagnostic")
+-- kLsp("<leader>dj", vim.diagnostic.goto_next, "next diagnostic")
+-- kLsp("<leader>dl", telescopeBi.diagnostics, "Telescope diagnostics")
+
+k.set("n", "<leader>dk", vim.diagnostic.goto_prev, {desc="prev diagnostic"})
+k.set("n", "<leader>dj", vim.diagnostic.goto_next, {desc="next diagnostic"})
+k.set("n", "<leader>dl", telescopeBi.diagnostics, {desc="Telescope diagnostics"})
 
 -- LSP shortcuts
 -- k.set("n", "<leader>f", vim.lsp.buf.format)
 -- k.set("n", "K", vim.lsp.buf.format, {buffer=0})
-kLsp("gt", vim.lsp.buf.type_definition, "type definition")
-kLsp("gr", vim.lsp.buf.rename, "rename")
-kLsp("gca", vim.lsp.buf.code_action, "code action")
+
+-- Stopped working:
+-- kLsp("gt", vim.lsp.buf.type_definition, "type definition")
+-- kLsp("gr", vim.lsp.buf.rename, "rename")
+-- kLsp("gca", vim.lsp.buf.code_action, "code action")
+k.set("n", "gt", vim.lsp.buf.type_definition, {desc="type definition"})
+k.set("n", "gr", vim.lsp.buf.rename, {desc="rename"})
+k.set("n", "gca", vim.lsp.buf.code_action, {desc="code action"})
+
+-- yank/put: " + macro + y/p
+-- useless macro: isusu€kb
+-- macro: cwk,€kb.setwa"n", $2F'2F"$2F"i{desc=$i}
 
 
 -- k.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
@@ -55,28 +70,32 @@ k.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 k.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 k.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gIc<Left><Left><Left><Left>]])
+-- * does the same thing k.set("n", "<leader>/", "/<C-r><C-w><CR>")
 k.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-k.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/frostynick/lazy.lua<CR>");
+k.set("n", "<leader>vpp",
+"<cmd>e " .. vim.fn.stdpath('config') .. "/lua/frostynick/lazy.lua<CR>");
 k.set("n", "<leader>vpr", "<cmd>e ~/p/Rhythm-Swipe<CR>");
 k.set("n", "<leader>vs", function() io.write('noooo <C-wv> instead\nsmh') end);
-
--- Git shortcuts
-k.set("n", "<leader>gr",
-"<cmd>!xdg-open $(git remote -v | awk '{ print $2 }' | head -n 1 | sed '$s/\\.git//')&<CR><CR>", { silent = true })
-k.set("n", "<leader>gf", vim.cmd.Git)
-k.set("n", "<leader>gl", "<cmd>Git log --oneline --decorate --graph --all<CR>")
-k.set("n", "<leader>gp", "<C-w>v<cmd>term<CR>igitp") -- if in git repository, open 1st remote url.
-k.set("n", "<leader>m", "<C-w>v<cmd>term<CR>igitp") -- if in git repository, open 1st remote url.
 
 -- xdg-open miscellaneous
 k.set("n", "<leader>o",
 "<cmd>!xdg-open .&<CR><CR>", { silent = true, desc = "Open working dir" })
 k.set("n", "<leader>p5",
-"<cmd>!xdg-open ~/p/p5-reference/index.html<CR><CR>", { silent = true })
+"<cmd>!xdg-open ~/p/p5-reference/index.html || xdg-open https://p5js.org/reference/<CR><CR>",
+{ silent = true })
+
+-- Git shortcuts
+k.set("n", "<leader>gr",
+"<cmd>!xdg-open $(git remote -v | awk '{ print $2 }' | head -n 1 | sed '$s/\\.git//')&<CR><CR>",
+{ silent = true })
+k.set("n", "<leader>gf", vim.cmd.Git)
+k.set("n", "<leader>gl", "<cmd>Git log --oneline --decorate --graph --all<CR>")
+k.set("n", "<leader>gd", "<cmd>Git diff<CR>")
+k.set("n", "<leader>gp", "<C-w>v<cmd>term<CR>igitp") -- if in git repository, open 1st remote url.
 
 -- Terminal shortcuts
-k.set("n", "<leader>t", "<C-w>v<cmd>term<CR>i")
+k.set("n", "<leader>t", "<C-w>v<cmd>term<CR>")
 k.set("n", "<leader>cr", vim.cmd.CompilerOpen) -- compiler run
 k.set("n", "<leader>ct", vim.cmd.CompilerToggleResults)
 -- k.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");"
