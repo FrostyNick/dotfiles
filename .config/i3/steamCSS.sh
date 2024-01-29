74a5b8d1757df1e3ea3270e95626a21da6ffc380
FrostyNick <57016218+FrostyNick@users.noreply.github.com>
2024-01-29 10:23:41 -0600

    Add steamCSS.sh for i3
    detects css file with -noverifyfiles flag (otherwise it will refuse to launch)

---
+++ .config/i3/steamCSS.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Credit: https://steamcommunity.com/sharedfiles/filedetails/?id=2921972296
+# will use this to add text to remind myself a fix that I need to add everytime.
+STEAM_DIR=$HOME/.local/share/Steam
+SUI_INDX=$STEAM_DIR/steamui/index.html
+if ( ! grep -q webkit\.css $SUI_INDX ); then
+	sed -i 's/<\/head>/\<link href="webkit.css" rel="stylesheet"><\/head>/' $SUI_INDX
+	echo "steamui/index.html edited"
+fi
+
+# For the final part, copy the contents of /usr/bin/steam
+# and add -noverifyfiles
+exec /usr/lib/steam/steam -noverifyfiles "$@"
