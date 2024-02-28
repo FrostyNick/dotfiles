d939bfff03a56bbba389dc6785dd1cd080cc6b07
FrostyNick <57016218+FrostyNick@users.noreply.github.com>
2024-02-27 23:04:33 -0600

    nv/lazy: nvim-cmp: <CR> -> <C-y>

--- .config/nvim/lua/frostynick/lazy.lua
+++ .config/nvim/lua/frostynick/lazy.lua
@@ -207,7 +207,7 @@ local plugins = {
                     ["<C-f>"] = cmp.mapping.scroll_docs(4),
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
                 }),
                 sources = cmp.config.sources({
                     { name = "nvim_lsp" },
