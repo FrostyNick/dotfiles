313553fb8b116a7cba6f51a1d1ae040640f81805
FrostyNick <57016218+FrostyNick@users.noreply.github.com>
2024-04-10 08:48:03 -0500

    i3: minor changes

--- .config/i3/steamCSS.sh
+++ .config/i3/steamCSS.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Credit: https://steamcommunity.com/sharedfiles/filedetails/?id=2921972296
+# not sure if useful for later: https://developer.valvesoftware.com/wiki/Steam_Skins
+#
 # will use this to add text to remind myself a fix that I need to add everytime.
 STEAM_DIR=$HOME/.local/share/Steam
 SUI_INDX=$STEAM_DIR/steamui/index.html
