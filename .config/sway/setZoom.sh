2c7284d1e5205bb6443b1ef693642da2769d6c56
FrostyNick <57016218+FrostyNick@users.noreply.github.com>
2024-10-02 21:20:15 -0500

    sway: add setZoom.sh + default zoom = 1.15

---
+++ .config/sway/setZoom.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# config - NOTE: This only works in sway. For wl-roots / hyprland, you can install wlr-randr
+
+# Example (assume you have one monitor in this case):
+# wlr-randr --output $(wlr-randr | head -n 1 | awk '{ print $1 }') --scale 1.1
+
+menu=$1
+[ -z $1 ] && menu=dmenu
+
+num=$(echo -e "0.7\n0.9\n1\n1.16\n1.3\n1.5\n2\n4" | $menu)
+
+[ $num ] && (swaymsg "output * scale $num") # || (notify-send "Canceled launching$num". || echo Canceled launching$num.) 
