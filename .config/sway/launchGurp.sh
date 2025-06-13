4484bb6f22cbd520881f0c4d015799fae0ce027b
FrostyNick <57016218+FrostyNick@users.noreply.github.com>
2025-06-13 12:59:14 -0500

    launchGurp.sh: Screenshots now pause + other improvements.
    - launchGurp.sh:
      - Main improvement: Screenshots now usually freeze (using the swayimg image viewer) when taking a screenshot.
      - bar is killed during screenshot. As a result in sway, this allows the whole screen to be used (less blurry screenshots), and is more immersive.
      - If dependencies are missing, notify user and exit if they're needed to run.
      - grim is themed to be dark green and black.
    - toggleBar.sh: Added -y and -n flags (1st argument only) to request enabling and disabling bar. This also helped launchGurp.sh functionality in return.
    - swayimg/config: Update config to have more visible help screen.

--- .config/sway/launchGurp.sh
+++ .config/sway/launchGurp.sh
@@ -1,21 +1,52 @@
 #!/usr/bin/env bash
 # launchFlameshot.sh - related
-# Dependencies: grim slurp swappy
-# Optional dependencies: tesseract (ocr) wl-clipboard (clipboard doesn't work without this)
-# Compared to Flameshot on Wayland (Hyprland/Sway), this is much more reliable and does similar.
+# Compared to Flameshot on Wayland (Hyprland/Sway), this is much more reliable and does similar. See ~/.config/swappy/config for swappy config.
+# WARNING:
+# - Depending on your setup, you may want to replace $bar variable below with your bar (waybar, swaybar, etc.) .. it gets default bar from $bar in sway config (or swaybar) at the moment.
+# - Not tested on multiple displays.
+# - Image holding is EXPERIMENTAL and not very reliable: If the image doesn't stay while taking a screenshot, you may need to increase the amount of time in sleep below / decrease quality of the first grim command that runs.
 
-# # [ -d "$HOME/Pictures" ] || mkdir -p "$HOME/Pictures"
-# s() { # unused code at the moment, which would work if swappy isn't installed
-#   # mkdir -p ~/Pictures # run first time
-#   cd "$HOME"
-#   name="slurp/$(date +%Y%m%d)_$(date +%T)_$(date +%N).jpeg"
-#   grim -c -t jpeg -g "$(slurp)" "$name" && notify-send "Saved to ~/$(echo $name | cut -c1-25)   .png"
-# }
+deps=("swayimg" "grim" "slurp" "awk" "jq") # swayimg can in theory be replaced with your preferred image viewer; preferably taking your full screen and allowing `grim` on top of full screen.
+ec=$(which notify-send 2> /dev/null || which echo)
 
-# TODO: Add pause while taking screenshot.
+for dep in "${deps[@]}"; do
+  if ! command -v "$dep" 2> /dev/null; then
+    $ec "launchGurp.sh error: $dep is not installed."; exit 1
+  fi
+done
+command -v wl-copy || ($ec "launchGurp: Image clipboard broken! Install wl-clipboard / wl-copy to fix." &)
 
-notify-send "General reminder: Check dependencies and config." & # i will add checks later if I remember
+live-screenshot() {
+  grim -c -t jpeg -g "$(slurp -b 000000cc -c 005500cc;kill $(ps aux | grep swayimg | grep -v grep | tail -n 1 | awk '{print $2}'))" - | swappy -f -
+}
 
-[ "$1" = "ocr" ] && ((notify-send "ocr" &); grim -g "$(slurp)" - | tesseract stdin stdout | wl-copy) || (grim -c -t jpeg -g "$(slurp)" - | swappy -f -) # || s || grimshot save area
+hold-screenshot() {
+  # grim -c -l 9 - | swayimg - # Quality from 0-9
+  grim -c - | swayimg -
+}
 
+full-screenshot() {
+  hold-screenshot &
+  # pid=$!
+  bar=$(grep "set \$bar" $HOME/.config/sway/config | cut -d ' ' -f 3- || echo swaybar)
+ # | rev | awk '{print $1}' | rev)
+  $HOME/.config/sway/toggleBar.sh -n "$bar"
+  barExit=$?
+  # wait "$pid"
+  sleep 0.8 # for some reason `live-screenshot` and `hold-screenshot` (either order) need to be around 0.6+ seconds apart if one of them is in the background to consistantly work with the delay in grim.
+
+  live-screenshot
+  if [ $barExit -eq 0 ]; then # success = it ran earlier, so bring it back like it was before
+    $HOME/.config/sway/toggleBar.sh -y "$bar"
+  fi
+}
+
+ocr-screenshot() {
+  if ! command -v tesseract 2> /dev/null; then
+    $ec "launchGurp: tesseract is needed in order to use ocr in this script."; exit 1
+  fi
+  ($ec "launchGurp: ocr" &); grim -g "$(slurp -b 000000cc -c 005500cc)" - | tesseract stdin stdout | wl-copy
+}
+
+[ "$1" = "ocr" ] && ocr-screenshot || full-screenshot
 
--- .config/sway/toggleBar.sh
+++ .config/sway/toggleBar.sh
@@ -1,12 +1,33 @@
 #!/usr/bin/env bash
 
-bar=$1
-if [ "$1" = "" ]; then
-  notify-send "No bar to toggle. Using i3bar."
+if [ "$1" = "-y" ]; then
+  prefBar=1;shift
+elif [ "$1" = "-n" ]; then
+  prefBar=0;shift
+else
+  prefBar=-1;
+fi
+
+bar="$@"
+if [ "$bar" = "" ]; then
+  $(which notify-send 2> /dev/null || which echo) "No bar to toggle. Using i3bar."
   bar="i3bar"
 fi
 
+if [ $prefBar -eq 1 ]; then
+  if [[ $(pgrep $bar) ]]; then
+    exit 1
+  fi
+  nm-applet &
+  $bar &
+  exit 0
+elif [ $prefBar -eq 0 ]; then
+  killall $bar || exit 1
+  killall nm-applet
+  exit 0
+fi
 
+# notify-send "bar toggle mode"
 if [[ $(pgrep $bar) ]]; then
   killall $bar
   killall nm-applet
