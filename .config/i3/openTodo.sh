14cb7811b70b9a92ae6bec46fcd6b1723e4e86cd
FrostyNick <57016218+FrostyNick@users.noreply.github.com>
2025-12-04 10:22:50 -0600

    Update i3/sway (desc)
    - i3/sway: Use dunst for notifications
      Controller support for i3/sway is doable if you use antimicrox.
      Don't print message on reload.
    - sway: keyboard shortcuts to extract ocr or qr code from screen
      sway: Experimental: botspot-screen-recorder for <c-o-r>
    - i3: setZoom.sh mapping
      i3: Improve GTK dark mode consistency.

--- .config/i3/openTodo.sh
+++ .config/i3/openTodo.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 $(which nvim || echo $HOME/.local/share/bob/nvim-bin/nvim) $HOME/dailytodo.md
-# [ -d ~/backup2022nov10/ ] && nvim ~/backup2022nov10/markor/totodo.md || nvim ~/backup2022nov/markor/totodo.md
+# [ -d ~/bk/ ] && nvim ~/bk/markor/totodo.md
 wait
